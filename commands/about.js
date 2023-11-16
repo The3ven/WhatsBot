33f516290415bc94a9ee619b26e881b74f54c2a7
fire7ly <fire7ly@gmail.com>
2023-11-17 02:42:00 +0530

    Fix holiday command & add new abou ďcommand
    Signed-off-by: fire7ly <fire7ly@gmail.com>

---
+++ commands/about.js
@@ -0,0 +1,36 @@
+//jshint esversion:8
+const execute = async (client, msg, args, isMe) => {
+  let msgMode = msg.to;
+  if (!isMe) {
+    msgMode = msg.from;
+  }
+  // console.log(args);
+  let Status_msg = args.join(" ");
+  try {
+    await client
+      .setStatus(Status_msg + " ~: By WhatsBot")
+      .then(
+        await client.sendMessage(
+          msgMode,
+          "```Status Successfully Updated " + `_${Status_msg}_` + "```"
+        )
+      );
+  } catch (e) {
+    await client.sendMessage(
+      msgMode,
+      `🙇‍♂️ *Error*\n\n` + "```Something unexpected while updating status```"
+    );
+  }
+
+  console.log(Status_msg);
+};
+
+module.exports = {
+  name: "about", //name of the module
+  description: "Sets the user's about message", // short description of what this command does
+  command: "!about", //command with prefix. Ex command: '!test'
+  commandType: "admin", // admin|info|plugin
+  isDependent: false, //whether this command is related/dependent to some other command
+  help: "set about type !about All is well", // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+  execute,
+};
--- commands/holiday.js
+++ commands/holiday.js
@@ -5,26 +5,13 @@ const csp = require("country-state-picker");
 let year = "";
 let month = "";
 let day = "";
-// const mon = {
-//   jan: 1,
-//   fab: 2,
-//   mar: 3,
-//   apr: 4,
-//   may: 5,
-//   jun: 6,
-//   jul: 7,
-//   aug: 8,
-//   sep: 9,
-//   oct: 10,
-//   nov: 11,
-//   dec: 12,
-// };
 let base_url = "https://calendarific.com/api/v2/holidays?&api_key=";
 
 const execute = async (client, msg, args, isMe) => {
   console.log("args.length", args.length);
   console.log("args", args);
   let response;
+  let holiday_search_data = "";
   year = "";
   month = "";
   day = "";
@@ -32,7 +19,8 @@ const execute = async (client, msg, args, isMe) => {
   console.log("month : ", month);
   console.log("year : ", year);
   if (args.length > 0) {
-    dmy_formatter(args[0]);
+    holiday_search_data = args[0].toUpperCase();
+    dmy_formatter(holiday_search_data);
   }
   let msgMode = msg.to;
   if (!isMe) {
@@ -41,58 +29,43 @@ const execute = async (client, msg, args, isMe) => {
   let date = new Date();
   let country = "";
   let type = "";
-  if (country.length <= 0 && country == "") {
-    country = csp
-      .getCountry("+" + `${msg.from.substring(0, 2)}`)
-      .code.toUpperCase();
-  }
-  if (month.length <= 0 && month == "") {
-    month = 1 + date.getMonth();
-    // } else {
-    //   let m = month.toLowerCase().substring(0, 3);
-    //   month = mon[m];
-  }
-  if (year.length <= 0 && year == "") {
-    year = date.getFullYear();
-  }
-  // if (day.length <= 0 && day == "") {
-  //   // day = date.getDate();
-  //   // day = 12;
-  // }
-  if (type.length <= 0 && type == "") {
-    type = "national";
-    // national - Returns public, federal and bank holidays
-    // local - Returns local, regional and state holidays
-    // religious - Return religious holidays: buddhism, christian, hinduism, muslim, etc
-    // observance - Observance, Seasons, Times
-  }
-  if (month == null) {
+
+  country.length <= 0 && country == ""
+    ? (country = csp
+        .getCountry("+" + `${msg.from.substring(0, 2)}`)
+        .code.toUpperCase())
+    : (country = "IN");
+  month.length <= 0 && month == "" && holiday_search_data.indexOf("Y") === -1
+    ? (month = 1 + date.getMonth())
+    : "";
+  year.length <= 0 && year == ""
+    ? (year = date.getFullYear())
+    : (year = "20" + year);
+  type.length <= 0 && type == ""
+    ? (type = "national")
+    : // national - Returns public, federal and bank holidays
+      // local - Returns local, regional and state holidays
+      // religious - Return religious holidays: buddhism, christian, hinduism, muslim, etc
+      // observance - Observance, Seasons, Times
+      "";
+
+  const errorMessage =
+    (month == null ? "Month" : "") +
+    (day == null ? "Day" : "") +
+    (year == null ? "Year" : "");
+
+  if (errorMessage) {
     await client.sendMessage(
       msgMode,
       `🙇‍♂️ *Error*\n\n` +
-        "```Month is passed but value is not specified```" +
+        "```" +
+        errorMessage +
+        "is passed but value is not specified```" +
         `${e}`
     );
     return;
   }
-  if (day == null) {
-    await client.sendMessage(
-      msgMode,
-      `🙇‍♂️ *Error*\n\n` +
-        "```Month is passed but value is not specified```" +
-        `${e}`
-    );
-    return;
-  }
-  if (year == null) {
-    await client.sendMessage(
-      msgMode,
-      `🙇‍♂️ *Error*\n\n` +
-        "```Month is passed but value is not specified```" +
-        `${e}`
-    );
-    return;
-  }
+
   if (Number(month) > 12) {
     await client.sendMessage(
       msgMode,
@@ -100,10 +73,15 @@ const execute = async (client, msg, args, isMe) => {
     );
     return;
   }
-  if (Number(day) > 31) {
+  let currentMonthDays = getDaysInMonth(month, year);
+
+  if (Number(day) > currentMonthDays) {
     await client.sendMessage(
       msgMode,
-      `🙇‍♂️ *Error*\n\n` + "```Month can`t be greater then 12```" + `${e}`
+      `🙇‍♂️ *Error*\n\n` +
+        "```Date can`t be greater then ```" +
+        `${currentMonthDays}` +
+        `${e}`
     );
     return;
   }
@@ -117,6 +95,7 @@ const execute = async (client, msg, args, isMe) => {
   console.log("day : ", day);
   console.log("month : ", month);
   console.log("year : ", year);
+
   let url = `${base_url}${HOLIDAY_API_KEY}&country=${country}&year=${year}&day=${day}&month=${month}&type=${type}`;
 
   console.log("URL : ", url);
@@ -140,22 +119,43 @@ const execute = async (client, msg, args, isMe) => {
         "```Something Unexpected Happened while fetching Holiday```" +
         `${response.data.response}`
     );
+    return;
   }
 
   console.log("data : ", response.data.response.holidays);
-  if (response.data.response.holidays.length > 1) {
-    response.data.response.holidays.forEach(async (Json) => {
-      console.log(Json);
+  try {
+    if (response.data.response.holidays.length > 1) {
+      response.data.response.holidays.forEach(async (Json) => {
+        console.log(Json);
+        await client.sendMessage(
+          msgMode,
+          `*Name :* _${Json.name}_\n*description :* _${Json.description}_\n*Date :* _${Json.date.datetime.day}-${Json.date.datetime.month}-${Json.date.datetime.year}_\n*Holiday Type :* ${Json.type[0]}`
+        );
+      });
+    } else {
+      let Json = response.data.response.holidays[0];
       await client.sendMessage(
         msgMode,
         `*Name :* _${Json.name}_\n*description :* _${Json.description}_\n*Date :* _${Json.date.datetime.day}-${Json.date.datetime.month}-${Json.date.datetime.year}_\n*Holiday Type :* ${Json.type[0]}`
       );
-    });
-  } else {
-    let Json = response.data.response.holidays[0];
+    }
+  } catch {
+    if (response.data.response.holidays.length === 0) {
+      let date = "";
+      (day.length > 0 ? (date += day + "-") : "") +
+        (month.length > 0 ? (date += month + "-") : "") +
+        (year.length > 0 ? (date += year) : "");
+      await client.sendMessage(
+        msgMode,
+        "No " + `${type}` + " holiday for date : " + `${date}`
+      );
+      return;
+    }
     await client.sendMessage(
       msgMode,
-      `*Name :* _${Json.name}_\n*description :* _${Json.description}_\n*Date :* _${Json.date.datetime.day}-${Json.date.datetime.month}-${Json.date.datetime.year}_\n*Holiday Type :* ${Json.type[0]}`
+      `🙇‍♂️ *Error*\n\n` +
+        "```Something Unexpected Happened while fetching Holiday```" +
+        `${response.data.response}`
     );
   }
   console.log("Im at  end");
@@ -173,6 +173,7 @@ const dmy_formatter = (str) => {
     month = parser(str, idx_M);
   }
   if (idx_Y >= 0) {
+    idx_M === -1 ? (month = "") : "";
     year = parser(str, idx_Y);
   }
 };
@@ -180,9 +181,14 @@ const dmy_formatter = (str) => {
 const parser = (str, idx) => {
   let value = str.substring(idx + 1, idx + 3);
   if (isNaN(value[0])) return null;
-  isNaN(value[1]) ? (value = "0" + value[0]) : "";
+  isNaN(value[1]) ? (value = value[0]) : "";
   return value;
 };
+
+function getDaysInMonth(month, year) {
+  const date = new Date(year, month + 1, 0);
+  return date.getDate();
+}
 
 module.exports = {
   name: "Holiday",
@@ -190,6 +196,6 @@ module.exports = {
   command: "!holiday", //command with prefix. Ex command: '!test'
   commandType: "plugin", // admin|info|plugin
   isDependent: false, //whether this command is related/dependent to some other command
-  help: "can tell about holiday of a day,week,month or year default is month\n\n*!holiday* [DDMMYYYY]\nEx: !holiday [D13M11Y2023]",
+  help: "can tell about holiday of a day,week,month or year default is month\n\n*!holiday* [DDMMYYYY]\nEx: !holiday [D13M11Y23]",
   execute,
 };
